bcf93c1565ac5190db17b9a5d75209b4757980e8
Harsh Jain <hvj.techture@gmail.com>
2022-01-13 10:42:02 +0530

    Food Order Inital Commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import mealsImage from '../assets/meals.jpg'
+import classes from './Header.module.css'
+
+function Header() {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h2>ReactMeals</h2>
+                <button>Cart</button>
+            </header>
+                <div className={classes['main-image']}>
+                    <img src={mealsImage} alt="ReactMeals Icon" />
+                </div>
+        </React.Fragment>
+    )
+}
+
+export default Header;
