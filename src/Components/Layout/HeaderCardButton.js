3995147cee99fd35afeba4c044cf9b122b3f4d8e
Harsh Jain <hvj.techture@gmail.com>
2022-02-23 07:50:27 +0530

    using context

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CartIcon from '../Cart/CartIcon';
+import cartContext from '../store/cart-context';
 import classes from './HeaderCardButton.module.css'
 
 function HeaderCardButton(props) {
+
+    const cartCtx = useContext(cartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce( (currentValue ,item) => { return currentValue + item.amount},0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>  Your Cart   </span>
-            <span className={classes.badge}>    3   </span>
+            <span className={classes.badge}> {numberOfCartItems} </span>
         </button>
     )
 }
--- src/Components/store/cart-context.js
+++ src/Components/store/cart-context.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 
 const cartContext = React.createContext({
   items:[],
