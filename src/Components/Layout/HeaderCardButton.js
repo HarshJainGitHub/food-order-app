3c5b72c6eb82bb9b625079fb2af113ef9185ee46
Harsh Jain <hvj.techture@gmail.com>
2022-03-05 08:16:23 +0530

    added animations

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,15 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartIcon from '../Cart/CartIcon';
 import cartContext from '../store/cart-context';
 import classes from './HeaderCardButton.module.css'
 
 function HeaderCardButton(props) {
-
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(cartContext);
     const numberOfCartItems = cartCtx.items.reduce( (currentValue ,item) => { return currentValue + item.amount},0);
 
+
+    const {items} = cartCtx;
+
+    useEffect( () =>{
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+        const timer = setTimeout( () => {
+            setBtnIsHighlighted(false);
+        },300)
+
+        return()=> {
+            clearTimeout(timer);
+        }
+    },[items]);
+
+    const btnStyles = `${classes.button} ${ btnIsHighlighted ? classes.bump : ''}`;
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnStyles} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
